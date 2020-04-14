06b17ccb0b8930be8ada369d854aa9d969986bbd
Gaurav <gauravgusain48@gmail.com>
2020-04-14 19:15:54 +0530

    Vision Structure

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/Vision'); //Vision is Db Name
+mongoose.connect('mongodb://localhost:27017/Vision',{useNewUrlParser: true,useUnifiedTopology: true}); //Vision is Db Name
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local');
+const sassMiddleware = require('node-sass-middleware')
 const session = require('express-session');
 
 app.use(express.static('./assets'));
+app.use(expressLayouts); //This for Setting partial in webpage
 app.use(express.urlencoded());
-
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+app.set('layout extractStyles', true);//this for setting css to particular page
+app.set('layout extractScripts', true);//javacript for particular page.
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
